349f1a192cb13353f1e6b7d33a1d688be171084c
SriramKalki <sriramkalki@hotmail.com>
2024-06-20 15:40:00 -0500

    Database integration and models setup

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,11 @@
-from flask import Blueprint, render_template, request, redirect, url_for
+from flask import Blueprint, render_template, request, redirect, url_for, flash
+from .models import Contact, db
 
 main = Blueprint('main', __name__)
 
-contacts = []
-
 @main.route('/')
 def index():
+    contacts = Contact.query.all()
     return render_template('index.html', contacts=contacts)
 
 @main.route('/add', methods=['GET', 'POST'])
@@ -14,6 +14,9 @@ def add_contact():
         name = request.form['name']
         email = request.form['email']
         phone = request.form['phone']
-        contacts.append({'name': name, 'email': email, 'phone': phone})
+        new_contact = Contact(name=name, email=email, phone=phone)
+        db.session.add(new_contact)
+        db.session.commit()
+        flash('Contact added successfully!', 'success')
         return redirect(url_for('main.index'))
     return render_template('add_contact.html')
